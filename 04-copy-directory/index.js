4ffdd997b4c1ac6b4e02d2c7073323cce739d080
Roman Bukharin <buxarin1999@gmail.com>
2022-05-25 11:59:18 +0600

    refactor: add rm function to 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
+const fsPromises = require('fs/promises');
 const { constants } = require('fs');
 const { copyFile } = require('fs/promises');
 const path = require('path');
 
 (async()=>{  
+  await fsPromises.rm(path.join(__dirname, 'copied-files'), {recursive: true, force: true});
   await fs.promises.mkdir(path.join(__dirname, 'copied-files'),{ recursive: true });
   fs.readdir(path.join(__dirname, 'files'), (error, data)=>{
     for (const file of data){
